dc718c02e95e43fafce917447d446a22b8344816
kou-practice <kouyou.kuroshita@artemis-tech.jp>
2020-05-22 03:32:30 +0000

    add role

--- resources/views/users/card.blade.php
+++ resources/views/users/card.blade.php
@@ -6,17 +6,28 @@
         <img class="rounded img-fluid" src="{{ Gravatar::src($user->email, 200) }}" alt="">
     </div>
     <div class="card-body">
-        @if ($user->age)
-        <p>年齢:{{ $user->age }}</p>
-        @endif
-        @if ($user->address)
-        <p>住所:{{ $user->address }}</p>
-        @endif
-        @if ($user->profile)
-        <p>プロフ</p>
-        <hr>
-        <p> {!! nl2br(e($user->profile)) !!}</p>
-        @endif
+        @can('admin-higher')
+            @if ($user->role == 1)
+                <h3 class="text-danger">権限:システム管理者</h3>
+            @elseif ($user->role == 5)
+                <h3 class="text-danger">権限:管理者</h3>
+            @else
+                <h3 class="text-danger">権限:ユーザー</h3>
+            @endif
+        @endcan
+        @can('user-higher')
+            @if ($user->age)
+            <p>年齢:{{ $user->age }}</p>
+            @endif
+            @if ($user->address)
+            <p>住所:{{ $user->address }}</p>
+            @endif
+            @if ($user->profile)
+            <p>プロフ</p>
+            <hr>
+            <p> {!! nl2br(e($user->profile)) !!}</p>
+            @endif
+        @endcan
     </div>
 </div>
 @include('user_follow.follow_button', ['user' => $user])
--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 
-Route::get('/', 'MicropostsController@index'); //上書き
+Route::get('/', 'MicropostsController@index');
 
 //ユーザ登録
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
@@ -22,7 +22,7 @@ Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
-// ユーザ機能
+// ユーザ機能　全権限で利用可
 Route::group(['middleware' => ['auth','can:user-higher']],function() {
     Route::group(['middleware' => 'auth'], function () {
         Route::resource('users', 'UsersController', ['only' => ['index', 'show', 'update', 'edit', 'destroy']]);
